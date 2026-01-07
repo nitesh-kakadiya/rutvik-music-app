2337b5a0693e7914d872a5544ad99e9346fa328e
Nitesh Kakadiya <niteshkakadiya50@gmail.com>
2026-01-07 22:06:39 +0530

    update 07-01-26: marquee and navbar solve

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,28 +5,30 @@ import SearchBar from "./SearchBar";
 
 export default function Navbar({ onSearch }) {
     return (
-        <header className="navbar">
-            {/* Brand / Logo */}
-            <div className="brand">
-                <NavLink to="/">MyMusic</NavLink>
-            </div>
+        <header className="navbar-wrap">
+            <nav class="navbar">
+                {/* Brand / Logo */}
+                <div className="brand">
+                    <NavLink to="/">MyMusic</NavLink>
+                </div>
 
-            {/* Navigation Links */}
+                {/* Navigation Links */}
 
-            <nav className="links">
-                <NavLink to="/" end className="nav-link">
-                    <FaHome size={22} />
-                </NavLink>
-                <NavLink to="/all-songs" className="nav-link">
-                    <FaMusic size={22} />
-                </NavLink>
-                <NavLink to="/myplaylist" className="nav-link">
-                    <FaList size={22} />
-                </NavLink>
+                <nav className="links">
+                    <NavLink to="/" end className="nav-link">
+                        <FaHome size={22} />
+                    </NavLink>
+                    <NavLink to="/all-songs" className="nav-link">
+                        <FaMusic size={22} />
+                    </NavLink>
+                    <NavLink to="/myplaylist" className="nav-link">
+                        <FaList size={22} />
+                    </NavLink>
+                </nav>
+
+                {/* Search Bar */}
+                <SearchBar onSearch={onSearch} />
             </nav>
-
-            {/* Search Bar */}
-            <SearchBar onSearch={onSearch} />
         </header>
     );
 }
